f55c903f317048ea2a0c32f42b79a328366f5005
adrianmend25 <adm008@ucsd.edu>
2022-05-01 14:55:08 -0700

    links added to lab report

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,18 +9,36 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int exMark = markdown.indexOf("!", currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
+            if (openBracket == -1){
+                break;
+            }
+            if (markdown.indexOf("!", currentIndex) != -1) {
+                int exMark = markdown.indexOf("!", currentIndex);
+                if ((exMark + 1) == openBracket) {
+                    currentIndex = markdown.indexOf(")", currentIndex) + 1;
+                    continue;
+                }
+            }
+            int exMark = markdown.indexOf("!", currentIndex);
             if ((exMark + 1) == openBracket){
                 currentIndex = markdown.indexOf(")", currentIndex) + 1;
                 continue;
             }
             int closeBracket = markdown.indexOf("]", openBracket);
+            if (closeBracket == -1) {
+                break;
+            }
             int openParen = markdown.indexOf("(", closeBracket);
+            if (openParen == -1) {
+                break;
+            }
             int closeParen = markdown.indexOf(")", openParen);
+            if (closeParen == -1) {
+                break;
+            }
             currentIndex = closeParen + 1;
             String url = markdown.substring(openParen + 1, closeParen);
             if (url.indexOf(" ") != -1) {
@@ -28,7 +46,6 @@ public class MarkdownParse {
             }
             toReturn.add(url);
         }
-
         return toReturn;
     }
 
--- test-file3.md
+++ test-file3.md
@@ -1,4 +1,4 @@
 # Title
-[link1](http://ok.com)
-some text in the middle
-[link2](......html)
+[link1](http://alr.com)
+[link2](therock.html)
+()()()()()()()()()()()()
